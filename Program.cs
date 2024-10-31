0bddbdb38fe09874a277bcf095e65dd4d74b3b74
alexb-bioid <a.bernst@bioid.de>
2024-10-31 15:18:26 +0100

    Refactoring

--- Program.cs
+++ Program.cs
@@ -1,75 +1,49 @@
+using BioID.Services;
 using Microsoft.IdentityModel.Tokens;
-using Serilog;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using static BioID.Services.BioIDWebService;
 
-namespace BioID.BWS.WebApp
+var builder = WebApplication.CreateBuilder(args);
+
+// Add gRPC bws client
+builder.Services.AddGrpcClient<BioIDWebService.BioIDWebServiceClient>(o =>
 {
-    public class Program
+    // Configure grpc server endpoint from appsettings.json
+    var gprcEndpoint = builder.Configuration.GetSection("BwsGrpcApiSettings")["Endpoint"] ?? throw new InvalidOperationException("The gRPC endpoint is not specified or is incorrect.");
+    o.Address = new Uri(gprcEndpoint);
+})
+    .AddCallCredentials((context, metadata, serviceProvider) =>
     {
-        public static void Main(string[] args)
-        {
-            // App Configuration
-            IConfiguration appConfiguration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
+        // Generate JWT token 
+        var key = builder.Configuration.GetSection("BwsGrpcApiSettings")["AccessKey"] ?? throw new InvalidOperationException("The grpc access key could not be found.");
+        var securityKey = new SymmetricSecurityKey(Convert.FromBase64String(key));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512);
+        var clientID = builder.Configuration.GetSection("BwsGrpcApiSettings")["ClientId"] ?? throw new InvalidOperationException("The grpc clientId could not be found.");
+        List<Claim> claims = [new Claim(JwtRegisteredClaimNames.Sub, clientID)];
+        var now = DateTime.UtcNow;
+        string token = new JwtSecurityTokenHandler().CreateEncodedJwt("demoWeApp", "bws", new ClaimsIdentity(claims), now, now.AddMinutes(10), now, credentials);
+        metadata.Add("Authorization", $"Bearer {token}");
+        return Task.CompletedTask;
+    });
 
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(appConfiguration)
-                .CreateLogger();
-            try
-            {
-                Log.Information("Bws Webapp is starting up.");
-                var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddRazorPages();
 
-                //Add support to logging with SERILOG
-                builder.Services.AddSerilog();
+var app = builder.Build();
 
-                // Add gRPC bws client
-                builder.Services.AddGrpcClient<BioIDWebServiceClient>(o =>
-                {
-                    // Configure grpc server endpoint from appsettings.json
-                    o.Address = new Uri(appConfiguration["BwsGrpcApiSettings:Endpoint"]!);
-                })
-                    .AddCallCredentials((context, metadata, serviceProvider) =>
-                    {
-                        // Generate JWT token 
-                        var securityKey = new SymmetricSecurityKey(Convert.FromBase64String(appConfiguration["BwsGrpcApiSettings:AccessKey"]!));
-                        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512);
-                        List<Claim> claims = [new Claim(JwtRegisteredClaimNames.Sub, appConfiguration["BwsGrpcApiSettings:ClientId"]!)];
-                        var now = DateTime.UtcNow;
-                        string token = new JwtSecurityTokenHandler().CreateEncodedJwt("demoWeApp", "bws", new ClaimsIdentity(claims), now, now.AddMinutes(10), now, credentials);
-                        metadata.Add("Authorization", $"Bearer {token}");
-                        return Task.CompletedTask;
-                    });
-
-                // Add services to the container.
-                builder.Services.AddRazorPages();
-
-                var app = builder.Build();
-
-                // Configure the HTTP request pipeline.
-                if (!app.Environment.IsDevelopment())
-                {
-                    app.UseExceptionHandler("/Error");
-                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                    app.UseHsts();
-                }
-                app.UseStaticFiles();
-
-                app.UseRouting();
-
-                app.MapRazorPages();
-
-                app.Run();
-            }
-            catch (Exception ex )
-            {
-                Log.Fatal(ex, "The application failed to start.");
-                Log.CloseAndFlush();
-            }
-            
-        }
-    }
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+app.UseStaticFiles();
+app.UseRouting();
+app.MapRazorPages();
+
+app.Run();
+
+
+
+
